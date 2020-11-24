b3e0a32ed5a88e5395d3d94a3ec65cb315eea2f9
tomrichard3011 <54161487+tomrichard3011@users.noreply.github.com>
2020-11-23 17:35:30 -0800

    Delete Test.java
    No tests needed for GUI

--- src/edu/sjsu/Team15/Test.java
+++
@@ -1,9 +0,0 @@
-package edu.sjsu.Team15;
-
-public class Test {
-    public static void main(String[] args) {
-        Login login = new Login();
-        //LoginDomain loginDomain = new LoginDomain();
-    }
-
-}
