b90330c5a651e63bc93e9485d7d157b75c8cc1e7
Nicolas Guerrero <Guerreronicolas1872@gmail.com>
2020-11-30 14:08:10 -0800

    Plan to add xml test cases
    Testing to see if the xml will work. Fast forward to next commit
    to see the tests implemented.

---
+++ src/XMLTest.java
@@ -0,0 +1,12 @@
+
+public class XMLTest {
+	public static void main(String[] args) {
+		// Create an xml file
+		
+		// Write to the xml
+		
+		// Read from the xml
+		
+		// Other query stuff
+	}
+}
