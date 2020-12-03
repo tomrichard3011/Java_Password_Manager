6cc33f8a143c1c6c4294add2efde64212df53dbc
Nicolas Guerrero <Guerreronicolas1872@gmail.com>
2020-12-03 12:14:47 -0800

    Add in early build for xml parsing and xml tests
    Minor fixes to the database handlers and some early tests for testing the xml.
    Not fully functional, use at your own risk.

--- src/edu/sjsu/Team15/DomainHandler.java
+++ src/edu/sjsu/Team15/DomainHandler.java
@@ -101,7 +101,9 @@ public class DomainHandler extends DatabaseHandler {
 		try {
 			TransformerFactory transformerFactory = TransformerFactory.newInstance();
 	        Transformer transformer = transformerFactory.newTransformer();
-	        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+	        //transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+	        transformer.setOutputProperty(OutputKeys.INDENT, "no");
+	        transformer.setOutputProperty(OutputKeys.ENCODING, "utf-8");
 	        DOMSource source = new DOMSource(doc);
 	        
 	        // I have no idea if this overwrites, I guess we'll find out the hard way
@@ -152,7 +154,9 @@ public class DomainHandler extends DatabaseHandler {
 		try {
 			TransformerFactory transformerFactory = TransformerFactory.newInstance();
 	        transformer = transformerFactory.newTransformer();
-	        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+	        //transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+	        transformer.setOutputProperty(OutputKeys.INDENT, "no");
+	        transformer.setOutputProperty(OutputKeys.ENCODING, "utf-8");
 	        source = new DOMSource(doc);
 		} catch (Exception e) {
 			e.printStackTrace();
