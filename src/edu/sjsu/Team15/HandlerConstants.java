b504cdfe13d3dd530fea150e3cde11dffb5d7fa8
Nicolas Guerrero <Guerreronicolas1872@gmail.com>
2020-12-05 17:09:58 -0800

    Clean up the database handler and adds function documentation
    The handlers are a fair bit cleaner, with more documentation.
    Additionally, files are now destroyed as soon as possible, removing
    some security issues. Finally, a few more constants were added.

--- src/edu/sjsu/Team15/HandlerConstants.java
+++ src/edu/sjsu/Team15/HandlerConstants.java
@@ -1,8 +1,13 @@
 package edu.sjsu.Team15;
 
 public class HandlerConstants {
+	// User File Constants
 	public static final int XMLUSER = 0;
 	public static final int XMLPASS = 1;
 	public static final int XMLCLIP = 2;
 	public static final int XMLPATH = 3;
+	// Domain File Constants
+	public static final int DOMAINNAME = 0;
+	public static final int DOMAINUSER = 1;
+	public static final int DOMAINPASS = 2;
 }
